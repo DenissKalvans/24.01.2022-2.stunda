aec6e38bd5693f94b5c8280332b561485eacff3c
DenisKalvan <95209747+DenissKalvans@users.noreply.github.com>
2022-01-24 07:32:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+text = input("Ievadit virkne tekstu: ")
+def Replace(tekst):
+  if len(tekst)>1:
+   sar = []
+   for burts in tekst:
+    sar.append(burts)
+    sar.reverse()
+    tekst = ""
+   for elements in sar:
+     tekst += elem
+     tekst = tekst.upper()
+     print (tekst)
+   else:
+     tekst = "Parak iss tekst"
+   print(tekst)
+replace(tekst)
